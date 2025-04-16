a946b0923690767c8d0e58efe14c1be1ace54e04
Pedro Luis de Souza Isidoro <105289528+pedro-isidoro@users.noreply.github.com>
2025-04-16 17:08:29 -0300

    feat:new-code

---
+++ Course/Course12/StringExtensionsCall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Course12.Extensions;
+namespace Course12
+{
+	public class StringExtensionsCall
+	{
+		public void Call()
+		{
+			string s1 = "Good morning dear students!";
+
+			Console.WriteLine(s1.Cut(10));
+		}
+	}
+}
+
