603d5a0c4a2e94f77d659118221b1e44ecf70d15
Pedro Luis de Souza Isidoro <105289528+pedro-isidoro@users.noreply.github.com>
2025-04-16 17:51:05 -0300

    feat:new-code

---
+++ Course/Course11/HashEntities/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Course11.HashEntities
+{
+	struct Point
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public Point (int x, int y): this()
+		{
+			x = x;
+			y = y;
+		}
+	}
+}
+
